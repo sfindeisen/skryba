1820320da9a020dc8109e9685dc63083345b3063
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2019-06-11 20:52:38 +0200

    implement collection utils (dict filtering)

---
+++ src/main/py/skryba/utils/collection.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def filter_dict(kv, p):
+    """Given a dictionary, returns a new one retaining only pairs satisfying the predicate."""
+    return { k:v for k,v in kv.items() if p(k,v) }
+
+def filter_dict_values_not_none(kv):
+    """Given a dictionary, returns a new one retaining only pairs where value is not None."""
+    return filter_dict(kv, lambda k, v : (v is not None))
