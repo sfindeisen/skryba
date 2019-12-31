d90410a3c6d8fc6c485dffab3b82a3594a72e482
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2019-12-31 22:34:25 +0100

    implement generic generator (gen)

---
+++ src/main/py/gen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import argparse
+
+from skryba.index import verbose, force_overwrite, warning, info, debug, parse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog='gen.py',
+        add_help=True, allow_abbrev=False, epilog="""This program comes with ABSOLUTELY NO WARRANTY.""")
+
+    parser.add_argument("--verbose", required=False, action="store_true", help="Verbose processing")
+    parser.add_argument("--overwrite-all", required=False, action="store_true", help="Overwrite all files without prompting (batch mode)")
+    parser.add_argument("--ctrl", required=True, metavar="FILE", help="Skryba control program")
+    parser.add_argument(metavar="output-dir", dest="outdir", help="Output directory")
+    args = parser.parse_args()
+
+    if (args.verbose):
+        verbose(True)
+    if (args.overwrite_all):
+        force_overwrite()
+
+    parse(args.ctrl)
