4cc7c24117e436cfb345c7e5db4ebdfda97f4642
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2019-12-31 22:32:31 +0100

    implement tokenizer

---
+++ src/main/py/skryba/lang/tokenizer.py
@@ -0,0 +1,44 @@
+tokens = (
+    'BIND',
+    'LAMBDA',
+
+    'EQUALS',
+    'RARROW',
+    'COMMA',
+
+    'LPAREN',
+    'RPAREN',
+
+    'IDENTIFIER',
+    'STRING_LITERAL'
+)
+
+t_BIND           = r'bind'
+t_LAMBDA         = r'lambda'
+t_EQUALS         = r'='
+t_RARROW         = r'->'
+t_COMMA          = r','
+t_LPAREN         = r'\('
+t_RPAREN         = r'\)'
+t_IDENTIFIER     = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_STRING_LITERAL = r'\"[^\n]*?\"'
+
+# Ignored characters
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_comment(t):
+    r'\#[^\n]*'
+    pass
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+# TODO disable debug
+import ply.lex
+ply.lex.lex(debug=True)
