ca021ff46d1d8e8da0948689b8aeb1af44e57184
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2020-01-03 18:22:54 +0100

    implement compiler (initial version)

--- src/main/py/skryba/lang/ast.py
+++ src/main/py/skryba/lang/ast.py
@@ -1,11 +1,23 @@
+from lang.env import Environment
+from utils.log import warning
+
 import abc
 
 class ASTNode(abc.ABC):
-    pass
+
+    def compile(self, env):
+        """Pre-run checks, including: identifiers, types."""
+        pass    # nothing to check
 
 class Program(ASTNode):
+
     def __init__(self, statement, program=None):
         self.statements = [statement] + ([] if (program is None) else program.statements)
+
+    def compile(self, env):
+        """Pre-run checks, including: identifiers, types."""
+        for s in self.statements:
+            s.compile(env)
 
 class Statement(ASTNode):
     pass
@@ -14,15 +26,24 @@ class Expression(ASTNode):
     pass
 
 class Bind(Statement):
+
     def __init__(self, identifier, expression):
         self.identifier = identifier
         self.expression = expression
 
+    def compile(self, env):
+        if (self.identifier in env):
+            warning("Re-bind not supported: {}".format(self.identifier))
+        else:
+            env[self.identifier] = self.expression
+
 class FunCallStmt(Statement):
+
     def __init__(self, funcall):
         self.funcall = funcall
 
 class FunCallExpr(Expression):
+
     def __init__(self, identifier, arguments):
         self.identifier = identifier
         self.arguments  = arguments
@@ -32,14 +53,17 @@ class Identifier(Expression):
         self.value = value
 
 class Lambda(Expression):
+
     def __init__(self, identifier, expression):
         self.identifier = identifier
         self.expression = expression
 
 class StringLiteral(Expression):
+
     def __init__(self, value):
         self.value = value
 
 class Tuple(Expression):
+
     def __init__(self, values):
         self.values = values
