652a80e8007b59c5fcebb679cefcb58eb9020c40
Stanislaw Findeisen <fixpoint.combinator@gmail.com>
2020-10-23 10:45:11 +0200

    implement german umlaut replacements (string2id)

--- src/main/py/skryba/utils/text.py
+++ src/main/py/skryba/utils/text.py
@@ -10,13 +10,33 @@ def normalize(s, allow_nonascii=True):
     else:
         return unicodedata.normalize('NFKD', s).encode('ascii', 'ignore').decode('ascii')
 
-def string2id(s, allow_nonascii=False):
+def string2id(s, allow_nonascii=False, de_umlaut_aware=True):
     """
     Converts an arbitrary string to a friendly ID (eg. a file name). Unicode
     normalization is applied, whitespace and non-word characters are removed
     or substituted.
     """
-    z = normalize(s, allow_nonascii)
+    z = s
+
+    # replace German umlauts with *e; replace scharfes s with ss
+    if de_umlaut_aware:
+        # scharfes s
+        z = z.replace('\u1E9E', 'SS')
+        z = z.replace('\u00DF', 'ss')
+
+        # a umlaut
+        z = z.replace('\u00C4', 'AE')
+        z = z.replace('\u00E4', 'ae')
+
+        # o umlaut
+        z = z.replace('\u00D6', 'OE')
+        z = z.replace('\u00F6', 'oe')
+
+        # u umlaut
+        z = z.replace('\u00DC', 'UE')
+        z = z.replace('\u00FC', 'ue')
+
+    z = normalize(z, allow_nonascii)
     z = re.sub(r'[^\w\s-]', '', z).strip()
     z = re.sub(r'[-\s]+', '-', z)
     debug("string2id: {} => {}".format(s,z))
